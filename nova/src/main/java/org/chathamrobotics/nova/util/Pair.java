2d691cb5ecc2f82543ed14298e506d4c61ad89c4
Carson Storm <stormymcstorm@gmail.com>
2018-02-15 14:32:42 -0500

    updated docs

--- nova/src/main/java/org/chathamrobotics/nova/util/Pair.java
+++ nova/src/main/java/org/chathamrobotics/nova/util/Pair.java
@@ -8,18 +8,35 @@ package org.chathamrobotics.nova.util;
  */
 @SuppressWarnings({"unchecked", "WeakerAccess"})
 public class Pair<F, S> extends Tuple {
+    /**
+     * Creates a new instance of {@link Pair}
+     * @param first     the first value
+     * @param second    the second value
+     */
     public Pair(F first, S second) {
         super(first, second);
     }
 
+    /**
+     * Gets the first value
+     * @return  the first value
+     */
     public F first() {
         return (F) values[0];
     }
 
+    /**
+     * Gets the second value
+     * @return  the second value
+     */
     public S second() {
         return (S) values[1];
     }
 
+    /**
+     * Clones the pair
+     * @return  the clone
+     */
     @Override
     public Pair clone() {
         return new Pair(values[0], values[1]);
