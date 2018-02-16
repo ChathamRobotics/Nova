aed1ca9a4695b07e5aea5838ced2ed9bbf90b520
Carson Storm <stormymcstorm@gmail.com>
2018-02-16 11:03:26 -0500

    added triplet test

---
+++ nova/src/test/java/org/chathamrobotics/nova/util/TripletTest.java
@@ -0,0 +1,71 @@
+package org.chathamrobotics.nova.util;
+
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(Enclosed.class)
+public class TripletTest {
+   public static class WithTest {
+       @Test
+       public void shouldCreateNewTriple() {
+           Triplet a = Triplet.with(1, 2, 3);
+           assertTrue(a != null);
+       }
+
+       @Test
+       public void shouldBeEquivalentToInstantiating() {
+           Triplet a = Triplet.with(1, 2, 3);
+           Triplet b = new Triplet<>(1, 2, 3);
+
+           assertEquals(a, b);
+       }
+   }
+
+   public static class ConstructorTest {
+       @Test
+       public void shouldCreate3Tuple() {
+           Triplet a = Triplet.with(1, 2, 3);
+           Tuple b = Tuple.with(1, 2, 3);
+
+           assertEquals(a, b);
+       }
+   }
+
+   public static class FirstTest {
+       @Test
+       public void shouldReturnFistValue() {
+           Triplet a = Triplet.with(1, 2, 3);
+           assertEquals(1, a.first());
+       }
+   }
+
+   public static class SecondTest {
+       @Test
+       public void shouldReturnSecondValue() {
+           Triplet a = Triplet.with(1, 2, 3);
+           assertEquals(2, a.second());
+       }
+   }
+
+   public static class ThirdTest {
+       @Test
+       public void shouldReturnThirdValue() {
+           Triplet a = Triplet.with(1, 2, 3);
+           assertEquals(3, a.third());
+       }
+   }
+
+   public static class CloneTest {
+       @Test
+       public void shouldReturnNewTriplet() {
+           Triplet a = Triplet.with(1, 2, 3);
+           Triplet b = a.clone();
+
+           assertFalse(a == b);
+           assertEquals(a, b);
+       }
+   }
+}
