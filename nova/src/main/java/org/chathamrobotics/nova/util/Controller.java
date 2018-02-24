900526dfd91daba5d907b389a6f5cf04b9eba667
Carson Storm <stormymcstorm@gmail.com>
2018-02-24 00:49:58 -0500

    remove callback setup

--- nova/src/main/java/org/chathamrobotics/nova/util/Controller.java
+++ nova/src/main/java/org/chathamrobotics/nova/util/Controller.java
@@ -348,16 +348,4 @@ public class Controller extends Gamepad {
 
         return ButtonState.RELEASED;
     }
-
-    private void setGamepadCallback(Gamepad gp, GamepadCallback callback) throws NoSuchFieldException, IllegalAccessException {
-        Field callbackField = Gamepad.class.getDeclaredField("callback");
-
-        try {
-            callbackField.setAccessible(true);
-
-            callbackField.set(gp, callback);
-        } finally {
-            callbackField.setAccessible(false);
-        }
-    }
 }
