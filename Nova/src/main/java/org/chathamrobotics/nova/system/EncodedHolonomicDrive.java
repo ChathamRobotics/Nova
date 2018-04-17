06a141134769b3232bb9b50aa93140e7bc4145cc
Carson Storm <stormymcstorm@gmail.com>
2018-04-17 14:33:13 -0400

    finished move and velocity logic

--- Nova/src/main/java/org/chathamrobotics/nova/system/EncodedHolonomicDrive.java
+++ Nova/src/main/java/org/chathamrobotics/nova/system/EncodedHolonomicDrive.java
@@ -17,12 +17,29 @@ import org.chathamrobotics.nova.util.units.AngleUnit;
 import org.chathamrobotics.nova.util.units.DistanceUnit;
 import org.chathamrobotics.nova.util.units.VelocityUnit;
 
-@SuppressWarnings("unused")
+@SuppressWarnings({"unused", "WeakerAccess"})
 public class EncodedHolonomicDrive extends HolonomicDrive {
+    public static final VelocityUnit DEFAULT_VELOCITY_UNIT = VelocityUnit.METER_PER_SECOND;
+    public static final AngleUnit DEFAULT_DIRECTION_UNIT = AngleUnit.RADIANS;
+    public static final DistanceUnit DEFAULT_DISTANCE_UNIT = DistanceUnit.METER;
+    public static final double DEFAULT_DELTA = 0;
+
+
     private final double wheelRadius;
     private final GyroSensor gyro;
     private final MotorEncoder frontLeftEncoder, frontRightEncoder, backRightEncoder, backLeftEncoder;
 
+    /**
+     * Creates a new instance of {@link EncodedHolonomicDrive}
+     * @param wheelDiameter the diameter of the wheel
+     * @param diameterUnit  the unit of measure for the diameter
+     * @param gyro          the gyroscope
+     * @param frontLeft     the front left motor
+     * @param frontRight    the front right motor
+     * @param backRight     the back right motor
+     * @param backLeft      the back left motor
+     * @param logger        the logger
+     */
     public EncodedHolonomicDrive(
             double wheelDiameter,
             DistanceUnit diameterUnit,
@@ -49,6 +66,42 @@ public class EncodedHolonomicDrive extends HolonomicDrive {
         backLeftEncoder = new MotorEncoder(backLeft);
     }
 
+    /**
+     * Sets the velocity for the robot
+     * @param velocity      the velocity
+     * @param direction     the direction
+     */
+    public void setVelocity(double velocity, double direction) {
+        setVelocity(velocity, DEFAULT_VELOCITY_UNIT, direction, DEFAULT_DIRECTION_UNIT);
+    }
+
+    /**
+     * Sets the velocity for the robot
+     * @param velocity      the velocity
+     * @param velocityUnit  the unit of measure for the velocity
+     * @param direction     the direction
+     */
+    public void setVelocity(double velocity, VelocityUnit velocityUnit, double direction) {
+        setVelocity(velocity, velocityUnit, direction, DEFAULT_DIRECTION_UNIT);
+    }
+
+    /**
+     * Sets the velocity for the robot
+     * @param velocity      the velocity
+     * @param direction     the direction
+     * @param directionUnit the unit of measure for the direction
+     */
+    public void setVelocity(double velocity, double direction, AngleUnit directionUnit) {
+        setVelocity(velocity, DEFAULT_VELOCITY_UNIT, direction, directionUnit);
+    }
+
+    /**
+     * Sets the velocity for the robot
+     * @param velocity      the velocity
+     * @param velocityUnit  the unit of measure for the velocity
+     * @param direction     the direction
+     * @param directionUnit the unit of measure for the direction
+     */
     public void setVelocity(double velocity, VelocityUnit velocityUnit, double direction, AngleUnit directionUnit) {
         velocity = velocityUnit.getTimeUnit().convertFrom(
                 MotorEncoder.DEFAULT_VELOCITY_UNIT.getTimeUnit(),
@@ -62,6 +115,19 @@ public class EncodedHolonomicDrive extends HolonomicDrive {
         backLeftEncoder.setVelocity(motorVelocities[3]);
     }
 
+    /**
+     * Gets the velocity of the robot
+     * @return  the velocity
+     */
+    public double getVelocity() {
+        return getVelocity(DEFAULT_VELOCITY_UNIT);
+    }
+
+    /**
+     * Gets the velocity of the robot
+     * @param velocityUnit  the unit to get the velocity in
+     * @return              the velocity
+     */
     public double getVelocity(VelocityUnit velocityUnit) {
         return MotorEncoder.DEFAULT_VELOCITY_UNIT.getTimeUnit().convertFrom(velocityUnit.getTimeUnit(), angularToLinear(calcMagnitude(
                 frontLeftEncoder.getVelocity(),
@@ -71,6 +137,37 @@ public class EncodedHolonomicDrive extends HolonomicDrive {
         ), MotorEncoder.DEFAULT_VELOCITY_UNIT.getAngleUnit(), velocityUnit.getDistanceUnit()));
     }
 
+    /**
+     * Moves the robot the given distance
+     * @param distance      the distance to move
+     * @param power         the maximum power to use while moving
+     * @param direction     the direction to move
+     */
+    public void move(double distance, double power, double direction) {
+        move(distance, DEFAULT_DELTA, DEFAULT_DISTANCE_UNIT, power, direction, DEFAULT_DIRECTION_UNIT);
+    }
+
+    /**
+     * Moves the robot the given distance
+     * @param distance      the distance to move
+     * @param distanceUnit  the unit of measure for the distance
+     * @param power         the maximum power to use while moving
+     * @param direction     the direction to move
+     * @param directionUnit the unit of measure for the direction
+     */
+    public void move(double distance, DistanceUnit distanceUnit, double power, double direction, AngleUnit directionUnit) {
+        move(distance, DEFAULT_DELTA, distanceUnit, power, direction, directionUnit);
+    }
+
+    /**
+     * Moves the robot the given distance
+     * @param distance      the distance to move
+     * @param delta         the distance tolerance
+     * @param distanceUnit  the unit of measure for the distance
+     * @param power         the maximum power to use while moving
+     * @param direction     the direction to move
+     * @param directionUnit the unit of measure for the direction
+     */
     public void move(double distance, double delta, DistanceUnit distanceUnit, double power, double direction, AngleUnit directionUnit) {
         distance = linearToAngular(distance, distanceUnit, MotorEncoder.DEFAULT_ROTATION_UNIT);
         delta = linearToAngular(delta, distanceUnit, MotorEncoder.DEFAULT_ROTATION_UNIT);
